e25be2fd80830ea645f546267930e68aa71dbe5f
Sam Thool <86099723+SamThool@users.noreply.github.com>
2025-09-01 11:53:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs= require("fs");
+const fs = require("fs");
 require("dotenv").config();
 const cors = require("cors");
 const connection = require("./config/config");
@@ -7,37 +7,42 @@ const routes = require("./routes");
 const path = require("path");
 const socketIO = require("socket.io");
 const http = require("http");
-const {initilizeSocket} = require("./utils/socket");
+const { initilizeSocket } = require("./utils/socket");
 const app = express();
 
 // ✅ Middleware he
 // app.use(cors({ origin: "*", credentials: true, optionSuccessStatus: 200 }));
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://mirai.isyncerp.com"
+  "https://mirai.isyncerp.com",
+  "https://crm-client-phi.vercel.app",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // ✅ Static Files & Routes
+app.get("/", (req, res) => {
+  res.send("✅ CRM Backend is running");
+});
+
 app.use("/api", routes);
 
-const imagesPath = path.join(__dirname, '/public/images');
+const imagesPath = path.join(__dirname, "/public/images");
 app.use("/api/images", express.static(imagesPath));
-
 
 // ✅ Create server from Express
 const server = http.createServer(app);
